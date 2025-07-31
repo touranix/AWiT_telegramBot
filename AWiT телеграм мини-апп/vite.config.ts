8313a078a261a4afb20a2faffb62363b2a0245fa
touranix <numismatist1970@gmail.com>
2025-07-31 19:33:50 +0300

    Update vite.config.ts

--- AWiT телеграм мини-апп/vite.config.ts
+++ AWiT телеграм мини-апп/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'public', // Указываем, что корень — папка public
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: '../dist', // Выходная папка относительно root
+  },
 });
